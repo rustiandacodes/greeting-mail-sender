1c7b7a99991504a434850f4e638eef592b0c3dd8
rustiandacode <rustiandazen09@gmail.com>
2023-05-01 14:08:11 +0700

    final

--- pages/api/mysql.ts
+++ pages/api/mysql.ts
@@ -3,7 +3,22 @@ import { dbConnection } from '@/config/db';
 export default async function handler(req: any, res: any) {
   const data = req.body;
 
-  const sql = "INSERT INTO '" + process.env.DB_NAME + "' VALUES ('" + data.senderName + "','" + data.senderEmail + "','" + data.recipientName + "','" + data.recipientEmail + "','" + data.message + "')";
+  const table = process.env.DB_TABLE;
+
+  const sql =
+    'INSERT INTO ' +
+    table +
+    " (senderName, senderEmail, recipientName, recipientEmail, message ) VALUES ('" +
+    data.senderName +
+    "','" +
+    data.senderEmail +
+    "','" +
+    data.recipientName +
+    "','" +
+    data.recipientEmail +
+    "','" +
+    data.message +
+    "')";
 
   dbConnection.query(sql, function (error: any, result: any) {
     if (error) {
